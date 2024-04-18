fd42b888b932ebe89f02f5000e76d463dde42088
Huandres Schmidt <163208912+HuanDreesL@users.noreply.github.com>
2024-04-18 08:46:21 -0300

    funcionario.dart

---
+++ funcionario.dart
@@ -0,0 +1,26 @@
+// Desenvolva uma classe `Funcionário` com os atributos nome, cargo e salário.
+// Implemente métodos para calcular aumento de salário e exibir as informações do funcionário.
+
+void main(){
+Funcionario colaborador = Funcionario('Huandres', 'Desenvolvedor', 500);
+
+print(colaborador.infoFuncionario());
+print(colaborador.aumentoSalarial(0.5));
+}
+
+class Funcionario{
+  String nome;
+  String cargo;
+  double salario;
+
+  Funcionario(this.nome,this.cargo,this.salario);
+
+String infoFuncionario(){
+  return "Nome: $nome\nCargo: $cargo\nSalário: $salario";
+}
+String aumentoSalarial(double aumento){
+  double salarioAumento = salario * aumento;
+  salarioAumento = salarioAumento + salario;
+  return "Novo salario com $aumento% de aumento fica: $salarioAumento";
+}
+}
