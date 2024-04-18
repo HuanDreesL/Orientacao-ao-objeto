bdd0b3110252d8b17dcddecaff0de4ec810661ef
Huandres Schmidt <163208912+HuanDreesL@users.noreply.github.com>
2024-04-18 09:10:10 -0300

    circulo.dart

--- circulo.dart
+++ circulo.dart
@@ -1,8 +1,8 @@
 void main(){
 
 Circulo circunferencia = Circulo(3);
-print(circunferencia.calcularAreaCirculo(3));
-print(circunferencia.circunferenciaCirculo(3));
+print(circunferencia.calcularAreaCirculo());
+print(circunferencia.circunferenciaCirculo());
 
 }
 class Circulo{
@@ -10,11 +10,11 @@ class Circulo{
 
   Circulo(this.raio);
 
-double calcularAreaCirculo(double raio){
+double calcularAreaCirculo(){
   double area = 3.14 * (raio * raio);
   return area; 
 }
-double circunferenciaCirculo(double raio){
+double circunferenciaCirculo(){
   double circunferencia = raio * (3.14 * 2);
   return circunferencia;
 }
